ab9cc22c4377f8217eca994809f04085108438a0
Hrithik7174 <104922107+Hrithik7174@users.noreply.github.com>
2026-08-20 15:21:51 +0530

    Delete Quick Quiz directory

--- Quick Quiz/api/index.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const { createQuizApp } = require("../quiz-app");
-
-const app = createQuizApp({ rootDir: path.join(__dirname, "..") });
-
-module.exports = async (request, response) => {
-  await app.handleRequest(request, response);
-};
--- Quick Quiz/start-quiz.ps1
+++
@@ -1,9 +0,0 @@
-$nodePath = "C:\Users\Hrithik Sadawarte\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe"
-
-if (-not (Test-Path $nodePath)) {
-  Write-Error "Bundled Node runtime not found at $nodePath"
-  exit 1
-}
-
-Set-Location $PSScriptRoot
-& $nodePath ".\server.js"
